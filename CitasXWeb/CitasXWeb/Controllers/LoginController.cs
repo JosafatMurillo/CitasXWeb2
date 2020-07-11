8e1db5277e60128bc524f5b04e03f08d22f21484
JosafatMurillo <josagears@hotmail.com>
2020-07-10 20:11:27 -0500

    Se arreglan los modelos

---
+++ CitasXWeb/CitasXWeb/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CitasXWeb.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CitasXWeb.Controllers
+{
+    [Route("login")]
+    public class LoginController : Controller
+    {
+
+        CitasXContext _context = new CitasXContext();
+
+        // GET api/<controller>/5
+        [HttpGet]
+        public async Task<IActionResult> Login([FromBody] string identificador, [FromBody] string contrasenia)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var usuario = _context.TbUsuario.FirstOrDefault(u => u.UsuIdentificador.Equals(identificador) && u.UsuContrasenia.Equals(contrasenia));
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+    }
+}
---
+++ CitasXWeb/CitasXWeb/Controllers/RegistrarPacienteController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitasXWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CitasXWeb.Controllers
+{
+    [Route("registrarPaciente")]
+    public class RegistrarPacienteController : Controller
+    {
+        CitasXContext _context = new CitasXContext();
+
+        // GET api/<controller>/5
+        [HttpPost]
+        public async Task<IActionResult> RegistrarPaciente([FromBody] TbUsuario paciente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var usuario = new TbUsuario();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+    }
+}
